d76f4bc10610c4ce58a8222648c04c169c1bb73d
SoyDony <danielduran1516@gmail.com>
2025-03-18 21:50:32 -0300

    agregue raza y edadAgregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DOO_Git1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+
+    public class perro
+    {
+        public string raza;
+        public int edad;
+    }
+}
